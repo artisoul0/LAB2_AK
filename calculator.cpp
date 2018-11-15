fa6d250931e2ae52e047295eb03d2fd76826fae6
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:45:44 +0200

    mark member-function as `static`
    In fact the `Calculator` class does not have member-variable, so we can
    mark `Add` and `Mul` as `static` and use them without an instance.
    
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,9 @@
 #include "calculator.h"
-
+//hello l am Yuriy
+//
+//
+//
+//
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
--- calculator.h
+++ calculator.h
@@ -4,8 +4,8 @@
 class Calculator
 {
     public:
-        int Add (double, double);
-        int Sub (double, double);
+        static int Add (int, int);
+        static int Mul (double, double);
 };
 
 #endif//CALCULATOR_H
